bc1a19c7d7e6a0f990b7b47158f24ba2d8d7fd0a
FelipeLevinir <levinir.f@gmail.com>
2021-05-27 15:13:45 -0400

    update

--- code/main.cc
+++ code/main.cc
@@ -192,24 +192,21 @@ int main(int argc, char** argv)
 	timer0.start();
 	MatrixToMem m1(fileName);
 	timer0.stop();
-	var0=var0+timer0.elapsed();
 	std::cout << "********************************************"<< std::endl;
-	std::cout << "Time to transfer to main memory: " << var0 << std::endl;
+	std::cout << "Time to transfer to main memory: " << timer0.elapsed() << std::endl;
 	std::cout << "--------------------------------------------"<< std::endl;
 /////////////////////////////////////////////////////////////////////////////////////////////	
 	timer1.start();
 	std::sort(m1._matrixInMemory, m1._matrixInMemory + m1._nfil);
 	timer1.stop();
-	var1=var1+timer1.elapsed();
-	std::cout << "Time to sort in main memory: " << var1 << std::endl;
+	std::cout << "Time to sort in main memory: " << timer1.elapsed() << std::endl;
 	std::cout << "********************************************"<< std::endl;
 /////////////////////////////////////////////////////////////////////////////////////////////
 	timer2.start();
 	MatrixToMem m2(fileName);
 	timer2.stop();
-	var2=var2+timer2.elapsed();
 	std::cout << "********************************************"<< std::endl;
-	std::cout << "Time to transfer to main memory: " << var2 << std::endl;
+	std::cout << "Time to transfer to main memory: " << timer2.elapsed() << std::endl;
 	std::cout << "--------------------------------------------"<< std::endl;
 /////////////////////////////////////////////////////////////////////////////////////////////	
 	timer3.start();
@@ -247,17 +244,14 @@ int main(int argc, char** argv)
 		break;
 	}
 	timer3.stop();
-	var3=var3+timer3.elapsed();
 ////////////////////////////////////////////////////////////////////////////////
 	timer4.start();
 	std::sort(m2._matrixInMemory, m2._matrixInMemory + m2._nfil);
 	timer4.stop();
-	var4=var4+timer4.elapsed();
-	
-	std::cout << "********************************************"<< std::endl;
-	std::cout << "Tiempo de ordenamiento vectorial "<< var3 <<std::endl;
+
+	std::cout << "Tiempo de ordenamiento vectorial "<< timer3.elapsed() <<std::endl;
 	std::cout << "--------------------------------------------"<< std::endl;
-	std::cout << "Tiempo de ordenamiento con sort "<< var4 <<std::endl;
+	std::cout << "Tiempo de ordenamiento con sort "<< timer4.elapsed() <<std::endl;
 	std::cout << "********************************************"<< std::endl;
 
 	return(EXIT_SUCCESS);
